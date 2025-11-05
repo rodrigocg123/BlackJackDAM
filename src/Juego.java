6a2fcad97d0fead9466ad1fbb7dd55d9e973f958
rodrigocg123 <rocasgut@gmail.com>
2025-11-05 09:38:41 +0100

    intercalado entre cuprier y jugador

--- src/Juego.java
+++ src/Juego.java
@@ -5,44 +5,44 @@ public class Juego {
         int ptsJugador = 0;
         int ptsBanca = 0;
         int LIMITE = 21;
-        int LimiteBanca = 16;
+        int limiteBanca = 16;
 
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Bienvenido al juego de BlackJack");
+        System.out.println("¡Bienvenido al juego de BlackJack!");
         System.out.println("Comienzan los turnos del jugador");
-        while (ptsJugador < LIMITE) {
+        while (ptsJugador < LIMITE || ptsBanca < limiteBanca) {
             System.out.println("Tus puntos actuales son: " + ptsJugador);
+            System.out.println("Puntos actuales de la banca: " + ptsBanca);
             System.out.print("¿Quieres pedir una carta? (si/no): ");
             String respuesta = entrada.nextLine();
-            
+            if (ptsBanca < limiteBanca) {
+                int cartaBanca = (int) (Math.random() * 10) + 1;
+                ptsBanca += cartaBanca;
+            }// fin if banca
+
             if (respuesta.equalsIgnoreCase("si")) {
-                int carta = (int) (Math.random() * 10) + 1;
-                ptsJugador += carta;
+                int cartaJugador = (int) (Math.random() * 10) + 1;
+                ptsJugador += cartaJugador;
             } else {
                 break;
-            }
+            }// fin if jugador
+        } // fin while
 
-        }//fin while jugador
-        if(ptsJugador > LIMITE){
-            System.out.println("Tus puntos son: "+ ptsJugador + " te has pasado de " + LIMITE + " puntos. Has perdido.");
-        }else{
-            System.out.println("Turno de la banca");
-            while (ptsBanca <= LimiteBanca) {
-                int carta = (int) (Math.random() * 10) + 1;
-                ptsBanca += carta;
-            }//fin while banca
-            System.out.println("Puntos del jugador: " + ptsJugador);
-            System.out.println("Puntos de la banca: " + ptsBanca);
-            if (ptsBanca > LIMITE || ptsJugador > ptsBanca) {
-                System.out.println("¡Felicidades! Has ganado.");
-            } else if (ptsJugador < ptsBanca) {
-                System.out.println("La banca gana. Has perdido.");
-            } else {
-                System.out.println("Empate.");
-            }
+        System.out.println("Puntos del jugador: " + ptsJugador);
+        System.out.println("Puntos de la banca: " + ptsBanca);
+        if (ptsBanca > LIMITE && ptsJugador < LIMITE) { // SE PASA LA BANCA
+            System.out.println("¡Felicidades! Has ganado.");
+        } else if (ptsJugador > LIMITE && ptsBanca < LIMITE) { // SE PASA EL JUGADOR
+            System.out.println("La banca gana. Has perdido.");
+        } else if (ptsJugador < ptsBanca) { // COMPARA PUNTUACIONES GANA BANCA
+            System.out.println("La banca gana. Has perdido.");
+        } else if (ptsJugador > ptsBanca) { // COMPARA PUNTUACIONES GANA JUGADOR
+            System.out.println("¡Felicidades! Has ganado.");
+        } else { // EMPATE
+            System.out.println("Empate.");
         }
-        entrada.close();
-    }//fin main
 
-}//fin clase
+        entrada.close();
+    }// fin main
+}// fin clase
